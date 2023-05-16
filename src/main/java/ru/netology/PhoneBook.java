92fcea57753fbafa2ca07bc7305f56ea4d8e10be
Garrik Ten <ten@otux.ru>
2023-05-16 06:19:40 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -2,5 +2,25 @@ package ru.netology;
 
 
 public class PhoneBook {
+	private static PhoneBook INSTANCE = null;
+
+
+	private PhoneBook() {};
+
+	public static PhoneBook getInstance() {
+		if (INSTANCE == null) {
+			synchronized (PhoneBook.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new PhoneBook();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+
+	public int add(String name, String phoneNumber)  {
+		return 0;
+	}
+	
 
 }
--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,5 +1,14 @@
 package ru.netology;
 
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
-	
+	@Test
+	public void testAddition() {
+		PhoneBook phoneBook = PhoneBook.getInstance();
+		int addResult = phoneBook.add("Ivan" , "+79251002032");
+		int unexpectedResult = 0;
+		assertNotEquals("Контакт добавлен", unexpectedResult, addResult);
+	}
 }
